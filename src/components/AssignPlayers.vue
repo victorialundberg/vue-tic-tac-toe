f0812fc81ad94ba1ce466c6d7b419313688d34b6
Victoria Lundberg <victoria.lundbrg@gmail.com>
2024-06-17 12:23:11 +0200

    :tada: First commit, file setup, props TicTacToe>AssignPlayers

---
+++ src/components/AssignPlayers.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Player } from '../models/Player';
+
+interface IPlayerProps {
+    players: Player[];
+};
+
+const props = defineProps<IPlayerProps>();
+
+console.log(props.players.length);
+
+
+
+const buttonText: string = "Nästa";
+const playerName = ref("");
+const handleSubmit = () => {
+    console.log("submit", playerName.value);
+    playerName.value = "";
+};
+</script>
+
+<template>
+    <div>
+        <form @submit.prevent="handleSubmit">
+            <input type="text" v-model="playerName" placeholder="Ange ditt namn">
+            <button>{{ buttonText }}</button>
+        </form>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/TicTacToe.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Player } from '../models/Player'
+import AssignPlayers from './AssignPlayers.vue'
+
+const players = ref<Player[]>([new Player("victoria", "X", 0), new Player("victoria", "X", 0)]);
+
+</script>
+
+<template>
+    <div v-for="(player, i) in players" :key="i">
+        {{ player.name }}
+    </div>
+    <AssignPlayers :players="players"></AssignPlayers>
+</template>
+
+<style scoped></style>
